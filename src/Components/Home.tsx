8b1ba6c37a4861aee64082c2d2a024511b251ba5
Dave <dave.e.siegel@gmail.com>
2019-09-26 15:23:40 -0400

    working on homepage. added temporary masthead background image

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home: React.FunctionComponent = () => {
-  return <h1>HOME PAGE</h1>;
+  return <div className="masthead"></div>;
 };
 
 export default Home;
